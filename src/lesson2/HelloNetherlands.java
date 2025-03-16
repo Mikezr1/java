582471617d90a06ace7848ae8cff8bb8c917d043
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-03-16 16:49:23 +0100

    Updated class name

--- src/lesson2/HelloNetherlands.java
+++ src/lesson2/HelloNetherlands.java
@@ -1,6 +1,6 @@
 package main.java.lesson2;
 
-public class HelloWorld {
+public class HelloNetherlands {
     public static void main(String[] args) {
         System.out.println("Hello Netherlands!");
     }
