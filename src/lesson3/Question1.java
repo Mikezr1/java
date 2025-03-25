0460bfd4ca24e87e038a1aa4905677a989f69062
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-03-25 15:44:18 +0100

    Question 1 answered

---
+++ src/lesson3/Question1.java
@@ -0,0 +1,10 @@
+package lesson3;
+
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Question: How many statements do you think a program can contain?");
+        System.out.println("Answer: A program can hold an unlimited amount of statements.\n" + //
+                        "By breaking the code up into multiple classes, methods, and modules, the computer can read the code more easily and require fewer resources.\n" + //
+                        "That said, the more RAM and system memory the computer has, the more it can handle.");
+    }
+}
