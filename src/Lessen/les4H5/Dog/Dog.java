d3ed6fe5019c8db29fc3b6b0d179478b7083ba0c
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-05-19 15:21:17 +0200

    Dog example

---
+++ src/Lessen/les4H5/Dog/Dog.java
@@ -0,0 +1,33 @@
+//package Lessen.les4H5.Dog;
+//
+//public class Dog {
+////    method parameter
+////    String breed
+//
+//    // scope: van { tot }
+//    void bark(boolean isScared) {
+//        if () {
+//
+//        }
+//    }
+//
+//    //andere methode, je kan niet bij die andere if scared vanaf hier (belangrijk)
+//    // je programme leest van boven naar beneden
+//
+//    void eat() {
+//        //if scared is dus buiten scope, amount is een lokale variabele, amount is nog niet gedefinierd.
+//        // int age is ook een lokale variabele
+//        int amount = 0;
+//        int amount = 10;
+//        //age mag je gebruiken in de loop, age mag je niet buiten de loop gebruiken.
+//        for (int i = 0; i < 10; i++) {
+//            int age = 10; //lokaal -> block scope variabele
+//            System.out.println(age);
+////            System.out.println(amount); //int mount = 10; bestaat al, mag niet nog een keer gemaakt worden.
+//
+//        }
+//    }
+//}
+//
+//
+////een variabele mag na een for loop wel maar niet ervoor. int amount bestond alleen in de anere scope. Dan mag je hem wel opniw defineren.
