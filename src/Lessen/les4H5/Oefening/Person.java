d08610b843635bfafe05cba75f8b8f56a03277a4
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-05-19 15:24:06 +0200

    Person example OOP

---
+++ src/Lessen/les4H5/Oefening/Person.java
@@ -0,0 +1,22 @@
+package Lessen.les4H5.Oefening;
+
+//Dit is de Person klasse met een Constructor en Methode.
+public class Person {
+
+    //Stap 1: Eigenschappen (variabelen) van de klasse declareren
+    String name ;
+    int age;
+
+    //Stap 2: Constructor -> Deze stelt de eigenschappen in zodat ze goed ingeladen kunnen worden in de klasse.
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    //Stap 3: Print methode -> methode die ervoor zorgt dat de eigenschappen geprint kunnen worden maken naar de klasse in de main method
+    void printDetails() {
+        System.out.println("Person - Name: " + this.name + " Age: " + this.age);
+    }
+
+}
+
