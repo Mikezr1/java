585ed531fdc941adf2cfcc2f869517f5f0a2e999
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-05-19 15:25:16 +0200

    Odd even test (not finished)

---
+++ src/Lessen/les4H5/MethodsOefening/OddEvenTest.java
@@ -0,0 +1,34 @@
+package Lessen.les4H5.MethodsOefening;
+/*Schrijf een boolean methode met de naam isOdd() in een klasse met de naam
+OddEvenTest, die een int als invoer neemt en true retourneert als deze oneven is.*/
+
+import java.util.Scanner;
+
+public class OddEvenTest {
+    public static void main(String[] args) {
+        //wat moet het doen
+        System.out.println("Which number do you want to check?");
+        Scanner scanner = new Scanner(System.in);// Scanne toevoegen
+
+//        System.out.println("This number is:" + isOdd ( even, int oneven));
+
+//        Waarde defeinieren
+        int oddNumber = scanner.nextInt();
+//        simpele methode
+        public int isOdd(int oddNumber){
+            for (int i = 0; i < ; i++) {
+                System.out.println(oddNumber);
+            }
+        }
+
+//        //Methode aanmaken
+//        public static int oddEvenTest (int even , int oneven){
+//            int even = 0;
+//            int oneven = 0;
+//
+//            if
+        }
+    }
+}
+
+
