5eb6d75ff6b5e73b1fedb7220eb27a480720062c
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-05-20 13:17:39 +0200

    Boolean methode made with class and modulo

--- src/Lessen/les4H5/MethodsOefening/OddEvenTest.java
+++ src/Lessen/les4H5/MethodsOefening/OddEvenTest.java
@@ -1,34 +1,30 @@
 package Lessen.les4H5.MethodsOefening;
-/*Schrijf een boolean methode met de naam isOdd() in een klasse met de naam
-OddEvenTest, die een int als invoer neemt en true retourneert als deze oneven is.*/
+/*Schrijf een boolean methode met de naam isOdd() in
+een klasse met de naam OddEvenTest,
+die een int als invoer neemt en true retourneert als deze oneven is.*/
 
-import java.util.Scanner;
+//methodes mogen nooit in een andere methode
 
 public class OddEvenTest {
+    //1 main hier wordt het programma gestart
     public static void main(String[] args) {
-        //wat moet het doen
-        System.out.println("Which number do you want to check?");
-        Scanner scanner = new Scanner(System.in);// Scanne toevoegen
+        //3 de invoer
+        int input = 3;
 
-//        System.out.println("This number is:" + isOdd ( even, int oneven));
-
-//        Waarde defeinieren
-        int oddNumber = scanner.nextInt();
-//        simpele methode
-        public int isOdd(int oddNumber){
-            for (int i = 0; i < ; i++) {
-                System.out.println(oddNumber);
-            }
-        }
-
-//        //Methode aanmaken
-//        public static int oddEvenTest (int even , int oneven){
-//            int even = 0;
-//            int oneven = 0;
-//
-//            if
-        }
+        //6 printen van de uitkomst
+        System.out.println(isOdd(input));
     }
+
+    //2 een boolean methode met de functienaam isOdd
+    public static boolean isOdd(int invoer) {
+        //4 argument meegeven met een int (wiskundige if formule)
+        boolean result = ((invoer) % 2 == 1);
+
+        //5 return variabele
+        return result;
+
+    }
+
 }
 
 
