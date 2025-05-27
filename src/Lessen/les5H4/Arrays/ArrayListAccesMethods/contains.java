21192a59ee97ca90e4032345924db68cdd749b76
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-05-27 13:36:28 +0200

    Arraylist modifiers trained

---
+++ src/Lessen/les5H4/Arrays/ArrayListAccesMethods/contains.java
@@ -0,0 +1,18 @@
+//package Lessen.les5H4.Arrays.ArrayListAccesMethods;
+//
+//public class contains {
+//    public static void main(String[] args) {
+//        // size()
+//        System.out.println("size(): " + lijst.size());  // Output: 4
+//
+//        // indexOf(Object o)
+//        System.out.println("indexOf(\"banaan\"): " + lijst.indexOf("banaan"));  // Output: 1
+//
+//        // contains(Object o)
+//        System.out.println("contains(\"kers\"): " + lijst.contains("kers"));  // Output: true
+//        System.out.println("contains(\"peer\"): " + lijst.contains("peer"));  // Output: false
+//
+//        // lastIndexOf(Object o)
+//        System.out.println("lastIndexOf(\"banaan\"): " + lijst.lastIndexOf("banaan")
+//    }
+//}
---
+++ src/Lessen/les5H4/Arrays/ArrayListAccesMethods/size.java
@@ -0,0 +1,13 @@
+package Lessen.les5H4.Arrays.ArrayListAccesMethods;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class size {
+    public static void main(String[] args) {
+        List<String> auto = connectedArrayList.haalAutoOp();
+
+        //het aantal auto's in de list/array
+        System.out.println(auto.size());
+    }
+}
